542c3ded3d0b62ad09014021cccd9c73522cba91
minjeong <0519suzy@naver.com>
2024-11-20 10:07:30 +0900

    five

--- src/p202214119/Main.java
+++ src/p202214119/Main.java
@@ -4,5 +4,11 @@ public class Main {
 	  public static void main(String[] args) {
 	    //  자신의 학번과 이름을 출력하라
 	    System.out.println("202214119 김민정");
+		System.out.println(Hello.sum(10));
+    	System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+    	System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+    	System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+
 	  }
 	}
