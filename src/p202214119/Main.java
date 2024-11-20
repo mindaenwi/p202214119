e1417bf08a566b8d010733ee6a129494c4843eb0
minjeong <0519suzy@naver.com>
2024-11-20 10:05:31 +0900

    four

--- src/p202214119/Main.java
+++ src/p202214119/Main.java
@@ -2,7 +2,13 @@ package p202214119;
 
 public class Main {
 	  public static void main(String[] args) {
-	    //  자신의 학번과 이름을 출력하라
-	    System.out.println("202214119 김민정");
+	    // 자신의 학번과 이름을 입력하라
+	    System.out.println("201514099 홍길동");
+	    System.out.println(Hello.sum(10));
+	    System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+	    System.out.println(World.factorial(3));
+	    System.out.println(World.factorial(6));
+	    System.out.println(World.factorial(9));
 	  }
 	}
